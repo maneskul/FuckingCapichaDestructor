cc20de957103c69fe5c916ca829e15ce11846314
Guilherme Maneskul <guilherme@maneskul.com.br>
2018-04-13 09:35:57 -0300

    Alterações na lógica de separação de letras para rodar os testes de output

--- FuckingCapchaDestructor/Program.cs
+++ FuckingCapchaDestructor/Program.cs
@@ -6,12 +6,12 @@
     using System.Drawing.Imaging;
     using System.IO;
     using System.Linq;
+    using System.Threading.Tasks;
     using OpenCvSharp;
     using OpenCvSharp.Extensions;
 
     public class Program
     {
-        public static string _currentFile { get; set; }
         const string inputDir = @"c:\Temp\Captchas\";
         const string outputDir = @"c:\Temp\Captchas\Output";
         const string inputDirSearchPattern = "*.jpg";
@@ -23,22 +23,25 @@
 
         public static void Main(string[] args)
         {
-            var files = Directory.GetFiles(inputDir, inputDirSearchPattern);
-            foreach (var file in files)
-            {
-                _currentFile = file;
-                var fileName = Path.GetFileName(_currentFile);
+            var files = Directory.GetFiles(inputDir, inputDirSearchPattern)
+                .OrderBy(d => Guid.NewGuid());
 
+            var ok = 0;
+            var error = 0;
+
+            Parallel.ForEach(files, file =>
+            {
+                var fileName = Path.GetFileName(file);
                 try
                 {
-                    RemoveNoises(_currentFile);
-                    Console.WriteLine($"=> Arquivo {fileName} - OK");
+                    RemoveNoises(file);
+                    Console.WriteLine($"=> Arquivo {fileName} - OK {++ok}");
                 }
                 catch
                 {
-                    Console.WriteLine($"=> Arquivo {fileName} - ERRO");
+                    Console.WriteLine($"=> Arquivo {fileName} - ERRO {++error}");
                 }
-            }
+            });
         }
 
         private static string RemoveNoises(string filePath)
@@ -53,8 +56,8 @@
                     captchaWithoutNoises.Save(path, ImageFormat.Jpeg);
 
                     var positions = GetEachLetter(captchaWithoutNoises);
-                    var expected = Path.GetFileNameWithoutExtension(_currentFile);
-                    SaveLetters(positions, expected, captchaWithoutNoises);
+                    var expected = Path.GetFileNameWithoutExtension(filePath);
+                    SaveLetters(filePath, positions, expected, captchaWithoutNoises);
 
                     return new ImageToText(captchaWithoutNoises).Resolve();
                 }
@@ -67,20 +70,27 @@
             var mat = original
                 .CvtColor(ColorConversionCodes.BGR2GRAY)
                 .Threshold(0, 255, ThresholdTypes.BinaryInv | ThresholdTypes.Otsu);
-            
+
             var countours = mat.FindContoursAsArray(RetrievalModes.External, ContourApproximationModes.ApproxTC89KCOS);
             var letterRegions = new List<LetterLocation>();
 
             foreach (var contour in countours)
             {
                 var rect = Cv2.BoundingRect(contour);
-                decimal proportion = (decimal)rect.Width / (decimal)rect.Height;
-                if (proportion > 1.25m)
-                {
-                    var halfWidth = rect.Width / 2;
 
-                    letterRegions.Add(new LetterLocation(rect.X, rect.Y, halfWidth, rect.Height));
-                    letterRegions.Add(new LetterLocation(rect.X + halfWidth, rect.Y, halfWidth, rect.Height));
+                // se a altura for menor que 18 ou se a largura for menor que 10, provavelmente se trata de um falso positivo
+                if (rect.Height < 18 || rect.Width < 10)
+                    continue;
+
+                if (countours.Count() != 5)
+                {
+                    decimal proportion = (decimal)rect.Width / (decimal)rect.Height;
+                    if (proportion > 2m)
+                    {
+                        var halfWidth = rect.Width / 2;
+                        letterRegions.Add(new LetterLocation(rect.X, rect.Y, halfWidth, rect.Height));
+                        letterRegions.Add(new LetterLocation(rect.X + halfWidth, rect.Y, halfWidth, rect.Height));
+                    }
                 }
                 else
                     letterRegions.Add(new LetterLocation(rect.X, rect.Y, rect.Width, rect.Height));
@@ -95,7 +105,8 @@
         }
 
         private static void SaveLetters(
-            List<LetterLocation> positions, 
+            string filePath,
+            List<LetterLocation> positions,
             string expected,
             Bitmap original)
         {
@@ -108,26 +119,24 @@
                 var letter = posAndLetter.Letter;
 
                 // extraimos a letra do arquivo original adicionando uma margem de 2 px
-                var img = mat[pos.Y - 2, pos.Y + pos.H + 2, pos.X - 2, pos.X + pos.W + 2];
-                var resize = img.Resize(new OpenCvSharp.Size(20, 20));
+                var img = mat[
+                    Math.Max(0, pos.Y - 2),
+                    Math.Min(original.Height, pos.Y + pos.H + 2),
+                    Math.Max(pos.X - 2, 0),
+                    Math.Min(original.Width, pos.X + pos.W + 2)];
+
+                var resize = img.Resize(new OpenCvSharp.Size(25, 25));
                 var bitmap = resize.ToBitmap();
 
                 // salvamos a letra numa pasta especifica com as letras
-                var directory = Path.Combine(Path.GetDirectoryName(_currentFile), "Data", letter.ToString());
+                var directory = Path.Combine(Path.GetDirectoryName(filePath), "Data", letter.ToString());
                 if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
 
                 var count = Directory.GetFiles(directory).Count() + 1;
-                var filename = $"{letter}_{count:000000}.jpg";
+                var filename = $"{letter}_{count:000000}_{expected}.jpg";
                 var path = Path.Combine(directory, filename);
-                bitmap.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+                bitmap.Save(path, ImageFormat.Jpeg);
             }
-        }
-
-        private static Bitmap GetLetter(Bitmap image, LetterLocation pos)
-        {
-            var mat = BitmapConverter.ToMat(image);
-
-            return mat[pos.Y - 2, pos.Y + pos.H + 2, pos.X - 2, pos.X + pos.W + 2].ToBitmap();
         }
 
         private class LetterLocation
